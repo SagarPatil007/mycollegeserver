a29389ce8b5762ab891b176f50a361aff0ea8d05
sagar patil <sagarpatil98754@gmail.com>
2023-09-22 13:06:45 +0530

    change the origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieparser = require("cookie-parser")
 
 
 const corsOptions = {
-  origin : "http://localhost:5173",//included origin as true
+  origin : "https://myclg.netlify.app/",//included origin as true
   credentials: true, //included credentials as true
 };
 
